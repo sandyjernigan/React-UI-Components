b0b5f1fd79fb3ac089ad02ec022d0b64c69553d6
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 22:24:46 -0400

    Added <CardContent /> component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
+// Create a <CardContent /> component that displays the card copy provided.
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div class="CardContent">
+            <h2>Get started with React</h2>
+            <p>React makes it painless to create interactive UIs.</p>
+            <p class="reference">reactjs.org</p>
+        </div>
+    );
+}
+
+export default CardContent;
