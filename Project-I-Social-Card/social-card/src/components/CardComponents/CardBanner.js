62ce48d5d6c74e9fb498ebc5f570469f6e9fe6cc
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 22:21:47 -0400

    Added <CardBanner /> component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,16 @@
+// Create a <CardBanner /> component that will display this image as it's background: 
+    // https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div class="CardBanner">
+            <img alt="Card Banner" 
+                src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" 
+            />
+        </div>
+    );
+}
+
+export default CardBanner;
