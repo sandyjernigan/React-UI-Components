6cce780ec742ec17c63610ab992ab2d542c8d561
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 21:29:11 -0400

    ImageThumbnail

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,6 +1,6 @@
-import React from 'react';
-// import './Header.css';
 // Create an <ImageThumbnail /> component using this image url https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png as it's img src.
+import React from 'react';
+import './Header.css';
 
 const ImageThumbnail = () => {
     return (
