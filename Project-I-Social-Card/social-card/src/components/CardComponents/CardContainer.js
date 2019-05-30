e443e2d2f6d664c1d737a573857aafb1bbf84781
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 22:47:02 -0400

    Setting up css styles

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -7,7 +7,7 @@ import CardContent from './CardContent';
 
 const CardContainer = () => {
     return (
-      <div onclick="location.href='https://www.reactjs.org';">
+      <div id="CardContainer" onclick="location.href='https://www.reactjs.org';">
           <CardBanner />
           <CardContent />
       </div>
