d17fd36750535fda749756823ae0e5c4e519d06d
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 22:15:00 -0400

    added HeaderContent Component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 const Header = () => {
     return (
       <header>
           <ImageThumbnail />
           <HeaderTitle />
+          <HeaderContent />
       </header>
     );
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,13 @@
+// Create a <HeaderContent /> component that displays the copy provided in the headers content.
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+    return (
+        <div class="HeaderContent">
+            Let's learn React by building simple interfaces with components.
+        </div>
+    );
+}
+
+export default HeaderContent;
