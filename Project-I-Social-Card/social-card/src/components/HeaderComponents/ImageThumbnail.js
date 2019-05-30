897ee6e6f3e9efb5a164c881ff11455bde360338
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 21:13:42 -0400

    changed logo image link

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ImageThumbnail = () => {
     return (
         <div class="ImageThumbnail">
-            <img alt="logo" src="https://ibin.co/3whrpKSBbZ81.png" />
+            <img alt="logo" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
         </div>
     );
 }
