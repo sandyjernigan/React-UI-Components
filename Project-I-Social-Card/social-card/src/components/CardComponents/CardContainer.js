fd1ff4f62297080d44fcc7d156c8deab73ea29c9
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-29 22:33:27 -0400

    Added <CardContainer/ > container component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
+// Create a <CardContainer/ > container component that will hold your card components.
+    // The entire <CardContainer /> should take a user to https://www.reactjs.org when clicked.
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = () => {
+    return (
+      <div onclick="location.href='https://www.reactjs.org';">
+          <CardBanner />
+          <CardContent />
+      </div>
+    );
+}
+
+export default CardContainer;
