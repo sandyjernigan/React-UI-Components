71098bde2469b18e5597d4f5a2ec08ef777ea85e
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-01 13:36:44 -0400

    Setting up CalculatorDisplay

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Display.css';
+
+const calculatorObj = {
+        display: 'comment',
+        add: 'function placeholder',
+        subtract: 'function placeholder',
+        multiply: 'function placeholder',
+        divide: 'function placeholder',
+        clear: 'function placeholder',
+        equals: 'function placeholder'
+    };
+
+const Calculator = () => {
+    return (
+      <div class="calculator">
+      </div>
+    );
+}
+export default Calculator;
