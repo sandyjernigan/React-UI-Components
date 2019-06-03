9224d4dd4c340451dad80a7c61a14a6a8846cb03
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-02 23:57:19 -0400

    Update CalculatorDisplay.js

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -11,7 +11,7 @@ const calculatorObj = {
         actionBar: [
             {
                 action: 'divide',
-                display: '/',
+                display: '÷',
                 function: 'function placeholder'
             },
             {
